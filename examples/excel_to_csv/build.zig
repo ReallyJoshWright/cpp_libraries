690292590020e6c2ad699efed2b9dfb802e74d17
ReallyJoshWright <picon2227@proton.me>
2025-08-03 04:39:32 -0500

    add build.zig for cross platform compilation

---
+++ examples/excel_to_csv/build.zig
@@ -0,0 +1,32 @@
+// zig build -Dtarget=x86_64-windows
+
+const std = @import("std");
+const Build = std.Build;
+
+pub fn build(b: *Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "app",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.addCSourceFiles(.{
+        .files = &.{
+            "main.cpp",
+        },
+        .root = b.path("."),
+        .flags = &.{
+            "-std=c++23",
+        },
+    });
+
+    exe.linkLibCpp();
+    exe.addIncludePath(b.path("../../include"));
+    exe.addLibraryPath(b.path("../../linux/lib"));
+    // exe.addLibraryPath(b.path("../../windows/lib"));
+    exe.linkSystemLibrary("excel_to_csv");
+    b.installArtifact(exe);
+}
